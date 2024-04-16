c5a443c81472ef3ec0b109ef84eb070e6b5f9025
hi@addison.codes <hi@addison.codes>
2024-04-16 12:12:38 -0400

    feat: copy and image changes

--- components/main.tsx
+++ components/main.tsx
@@ -9,24 +9,29 @@ export function Main() {
   return (
     <section className="flex flex-col items-center justify-start
      min-h-screen bg-white">
-      {/* <Image
+      <Image
         alt="Logo"
         className="aspect-[2/1] overflow-hidden rounded-lg object-contain object-center"
         height="70"
-        src="/BIM-primary-logo-1.png"
+        src="/Armor_Logo.png"
         width="500"
-      /> */}
-      {/* <p className="text-center text-zinc-500 w-1/2 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400 mb-12">
-        We are excited to announce that Maine Feldenkrais & Physical Therapy has joined the Back in Motion Physical Therapy family. This partnership will allow us to expand our services and better serve our community. <br/><br/>
+      />
+      <p className="text-center text-zinc-500 w-1/2 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-zinc-400 mb-12">
+        We're thrilled to let you know we are officially joining the Armor Physical Therapy family in order to better serve the Battle Creek area! We look forward to being fully immersed with this established and well-respected provider. We feel we can best expand our services and play a larger role in helping treat you by making this transition. <br /><br />
 
-The physical therapists at Back in Motion Physical Therapy have advanced training in a variety of areas, so we can help you with even the most complex musculoskeletal injuries. Whether you’re getting back to work, playing sports, or simply trying to complete everyday activities, we can help you reach your full potential. <br/><br/>
+        For you, as a valued patient, nothing will change. You will still visit the same clinic to work with the same therapists. We will continue to offer our physical, speech, and occupational therapy services as normal. <br /><br />
 
-For our existing patients, nothing will change. You will still be able to visit the same clinic and work with the same physical therapists. <br/><br/>
+        The entire Muscle & Spine Rehab team deeply appreciates the trust you've placed in us over the years. We're eager to embark on this new chapter as part of the Armor Physical Therapy family and hope you'll journey with us. <br /><br />
 
-We are very grateful for your trust and support, and we look forward to continuing to serve you as part of the Back in Motion Physical Therapy family. 
-      </p> */}
+        We are very grateful for your trust and support, and we look forward to continuing to serve you as part of the Armor Physical Therapy family.<br /><br />
+
+        2545 Capital Ave SW STE 140<br />
+        Battle Creek, MI 49015<br />
+        Fax: (269) 979-3000<br />
+        Phone: <a href="tel:269-248-1958">(269) 248-1958</a><br />
+      </p>
       <Link
-        className="inline-flex h-10 items-center justify-center rounded-md bg-cyan-900 px-16 py-8 text-xl font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-cyan-900 dark:hover:bg-cyan-900 dark:focus-visible:ring-zinc-300"
+        className="inline-flex h-10 items-center justify-center rounded-md bg-[#19b0df] px-16 py-8 text-xl font-medium text-zinc-50 shadow transition-colors hover:bg-zinc-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50"
         href="https://armorpt.com/location/physical-therapy-battle-creek-lakeview-mi/"
       >
         Continue to Armor Physical Therapy - Battle Creek Lakeview
